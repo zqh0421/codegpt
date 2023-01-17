cd05731d32134820a2573a937f3280578d1000db
zqh0421 <zqh0421@hotmail.com>
2023-01-17 11:15:47 +0800

    Fix problems

--- src/components/PromptEditor/PromptEditor.tsx
+++ src/components/PromptEditor/PromptEditor.tsx
@@ -1,6 +1,6 @@
 // 后面预备添加拖拽文件上传文本，但是需要对文件文本进行分析，所以暂时就没有加这个模块了，计划先把基本的做完
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Input } from 'antd'
 
 const { TextArea } = Input
@@ -13,11 +13,28 @@ const PromptEditor: React.FC<Props> = (Props) => {
   const changePrompt = (value : string) => {
     Props.setPrompt(value)
   }
+  const defaultValue : string = ("var fibonacci = function(){\n"+
+  "  var memo = [0,1];\n"+
+  "  var fib = function(n){\n"+
+  "    var result = memo[n];\n"+
+  "    if(typeof result !== 'number'){\n"+
+  "      result = fib(n-1)+fib(n-2);\n"+
+  "      memo[n] = result;\n"+
+  "    }\n"+
+  "   return result\n"+
+  "  };\n"+
+  "  return fib;\n"+
+  "}()");
+  
+  useEffect(() => {
+    Props.setPrompt(defaultValue)
+  }, [])
+  
   
   return (
     <>
       <TextArea
-        defaultValue={"test."}
+        defaultValue={defaultValue}
         rows={4}
         placeholder="Please write or copy your code here."
         style={{
